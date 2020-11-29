b21fbf233f61456307d66199999c9ccdeb5f1d17
Andrew Eames <ajerxs@gmail.com>
2020-11-29 23:52:24 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,8 +2,14 @@ class Author
 
   attr_accessor :name
 
+  @@post_count = []
+
   def initialize(name)
     @name = name
+  end
+
+  def posts
+    Post.all.select {|post| post.author = self}
   end
 
 end
